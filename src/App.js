87fcf586c81ad48d58b073cecf34b668e8578e45
subediashish <subedi.ashish@protonmail.com>
2021-08-26 11:09:32 +0545

    Header Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-          <h1>Amazon Clone</h1>
+    <div className="app">
+       <Header />
     </div>
   );
 }
