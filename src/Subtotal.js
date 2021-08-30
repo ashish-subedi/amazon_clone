b1ae2775975f4008b6e2e45b647bcc06149d2f83
subediashish <subedi.ashish@protonmail.com>
2021-08-30 14:36:40 +0545

    Checkout_Products_add_remove

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import  './Subtotal.css'
-import CurrencyFormat from 'react-currency-format'
-import { useStateValue } from './StateProvider';
-import { getBasketTotal } from './reducer';
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
-  const [{basket},dispatch]= useStateValue();
-  
+  const history = useHistory();
+  const [{ basket }, dispatch] = useStateValue();
 
-    return (
-        <div className="subtotal">
-            <CurrencyFormat
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
         renderText={(value) => (
-          <> 
+          <>
             <p>
               {/* Part of the homework */}
               Subtotal ({basket.length} items): <strong>{value}</strong>
@@ -29,9 +30,8 @@ function Subtotal() {
         prefix={"$"}
       />
 
-        <button>Proceed to Checkout</button>   
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Subtotal
+export default Subtotal;
