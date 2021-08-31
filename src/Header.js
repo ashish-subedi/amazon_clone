dd3df382bd55fb45a67c592cd3e8b6bd825f639e
ashish-subedi <ashishsubedinp@gmail.com>
2021-08-31 09:17:23 +0545

    loginFE

--- src/Header.js
+++ src/Header.js
@@ -25,10 +25,13 @@ function Header() {
             </div>
 
             <div className="header__nav">
+                <Link to='/login'>
                 <div className="header__option">
                         <span className="header__optionLineOne">Hello User</span>
                         <span className="header__optionLineTwo">Sign IN</span>
                 </div>
+                </Link>
+
 
                 <div className="header__option">
                 <span className="header__optionLineOne">Returns</span>
